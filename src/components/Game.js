cc04c9be94139fe66703f6d94b56b5907fb64c9e
Sutton Ashby <korim_fidelis@yahoo.com>
2018-07-16 00:17:25 -0400

    randomized render

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,11 +10,26 @@ export default class Game extends Component {
         score: 0
     }
 
+    shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]]; 
+        }
+    }
+
+
     render() {
+
+        const shuffled = this.shuffleArray(this.state.data)
+
+        const forDisplay = this.state.data.map((flag) =>{
+            return <Tile key={flag.id} id={flag.id} name={flag.name}  image={flag.image}/>
+        })
         return <div>
             <div>Score: {this.state.score}</div>
             <div> game space
-                <Tile id={data[0].id} name={data[0].name}  image={data[0].image}/>
+                {/* <Tile id={data[0].id} name={data[0].name}  image={data[0].image}/> */}
+                {forDisplay}
             </div>
 
 
